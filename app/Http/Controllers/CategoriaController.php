be6345d7d26831765edd5de85b9dc2d084ad7561
WiliamDida1 <wiliamccanccapaquiro@gmail.com>
2025-11-24 14:23:13 -0500

    cambios

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    //crud
+    //listar
+    public function listarCategoria(){
+        //$categorias = Categoria::all();
+        //$categorias = Categoria::orderBy('created_at','asc')->get();
+        $categorias = categoria::first();
+        return response()->json(
+            [
+                'success'=> true,
+                'nombre' => 'Wiliam Dida',
+                'data' => $categorias,
+            ]
+            );
+    }
+    public function agregarCategoria(){
+        $categorias = Categoria::orderBy();
+    }
+    public function editarCategoria(){
+        
+    }
+    public function eliminarCategoria(){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -15,13 +16,13 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('hola-mundo', function () {
-    
     return Inertia::render('HolaMundo');
 })->middleware(['auth', 'verified'])->name('hola-mundo');
 
 Route::get('contador', function () {
-    
     return Inertia::render('Contador');
 })->middleware(['auth', 'verified'])->name('contador');
+
+Route::get('categorias-data',[CategoriaController::class, 'listarCategoria']);
 
 require __DIR__.'/settings.php';
